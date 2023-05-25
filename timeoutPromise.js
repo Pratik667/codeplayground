5b2d5b81e39c57a14b436592daad43e57ea89231
pratik__6696 <pratik.mandlik@noesis.tech>
2023-05-25 20:05:44 +0530

    Promise that resolves after the specified time has passed

--- timeoutPromise.js
+++ timeoutPromise.js
@@ -1,7 +1,15 @@
 // Implement a function that takes a time (in milliseconds) and returns a Promise that resolves after the specified time has passed
 
-async function resolveIn(value, milliseconds) {
-    return new Promise(resolve => setTimeout(() => resolve('success'), milliseconds));
+async function resolveIn(milliseconds) {
+    console.log("Promise is pending");
+    let timer = new Promise((resolve) => {
+        setTimeout(() => {           
+            resolve('success!!');
+            console.log(`Promise is resolved after ${milliseconds}`);
+        }, milliseconds);  
+    });
+    return timer;
 }
 
-resolveIn("hello", 5000);
+resolveIn("hello", 5000).then(value => console.log(value)).catch(err => console.error(err));
+//.then will print the resolve value
